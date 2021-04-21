e4861b3b4ff33d69e20dea2f1b2de178381a396e
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-04-21 20:55:18 -0300

    rxjs for statistics

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,34 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 import { MathValidators } from '../math-validators';
-
+import { delay, filter, scan } from 'rxjs/operators';
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
-  mathForm = new FormGroup({
-    a: new FormControl(this.randomNumber()),
-    b: new FormControl(this.randomNumber()),
-    answer: new FormControl("")
-  },[
-     MathValidators.isCorrect("answer","a","b")
-  ])
-  constructor() { }
+  secondsPerSolution:number=0;
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl(''),
+    },
+    [MathValidators.isCorrect('answer', 'a', 'b')]
+  );
+  constructor() {}
 
   ngOnInit(): void {
+    this.mathForm.statusChanges
+      .pipe(
+        filter((value) => value === 'VALID'),
+        delay(100), //ms
+        scan((acc,value)=>{
+          return{
+            numberSolved: acc.numberSolved+1,
+            startTime: acc.startTime
+          }
+        },{numberSolved:0,startTime:new Date()}),
+      )
+      .subscribe(({numberSolved,startTime}) => {
+        this.secondsPerSolution=(
+          new Date().getTime() - startTime.getTime()
+        ) / numberSolved / 1000
+
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: '',
+        });
+      });
   }
 
-  randomNumber():number{
-    return Math.floor(Math.random() * 10)
+  randomNumber(): number {
+    return Math.floor(Math.random() * 10);
   }
-  
-  get a(){
+
+  get a() {
     return this.mathForm.value.a;
   }
-  get b(){
+  get b() {
     return this.mathForm.value.b;
   }
-  
 }
