e806300cf61191316a8567a4c59128644f3bacdf
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-04-21 19:52:00 -0300

    displaying form values

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
-
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl("")
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  randomNumber():number{
+    return Math.floor(Math.random() * 10)
+  }
+  
+  get a(){
+    return this.mathForm.value.a;
+  }
+  get b(){
+    return this.mathForm.value.b;
+  }
+  
 }
